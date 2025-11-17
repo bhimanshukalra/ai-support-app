d6dba331802706760e02c8e84672825febcc1c92
bhimanshukalra <bhimanshukalra@gmail.com>
2025-11-17 11:56:38 +0530

    Chore: added apps/widget & packages/math

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,13 +1,13 @@
-import { add } from "@workspace/math/add";
 import { Button } from "@workspace/ui/components/button";
+import { add } from "@workspace/math/add";
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello apps/web</h1>
+        <h1 className="text-2xl font-bold">Hello apps/widget</h1>
         <Button size="sm">Button</Button>
-        <p>{add(2, 5)}</p>
+        <p>{add(5, 5)}</p>
       </div>
     </div>
   );
