304d5ec3b8ee513d0525286e15b7cadc516b16ee
bhimanshukalra <bhimanshukalra@gmail.com>
2025-11-17 22:51:56 +0530

    Chore: connected web and widget to backend

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,16 +1,20 @@
-import { add } from "@workspace/math/add";
+"use client";
+
+import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
-import { Input } from "@workspace/ui/components/input";
+import { useMutation, useQuery } from "convex/react";
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
+  const addUser = useMutation(api.users.add);
+
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello apps/web</h1>
-        <Button size="sm">Button</Button>
-        <p>{add(2, 5)}</p>
-        <Input />
-      </div>
+    <div className="flex items-center justify-center min-h-svh flex-col">
+      <h1 className="text-2xl font-bold">Hello apps/web</h1>
+      <Button onClick={() => addUser()}>Add</Button>
+      {users?.map((currentUser) => (
+        <p key={currentUser._id}>{JSON.stringify(currentUser)}</p>
+      ))}
     </div>
   );
 }
--- apps/web/components/providers.tsx
+++ apps/web/components/providers.tsx
@@ -1,18 +1,10 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { ReactNode } from "react";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 
-export function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <NextThemesProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-      enableColorScheme
-    >
-      {children}
-    </NextThemesProvider>
-  )
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL || "");
+
+export function Providers({ children }: { children: ReactNode }) {
+  return <ConvexProvider client={convex}>{children}</ConvexProvider>;
 }
